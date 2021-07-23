c42634c2bc3612b5ccdcd6cf9650eaf08dff4fe2
Nima Rasooli <nimarasooli1@gmail.com>
2021-07-23 23:58:44 +0430

    Updated all of the methods for future codes.

--- src/dab/src/lib.rs
+++ src/dab/src/lib.rs
@@ -4,46 +4,34 @@ use ic_cdk::*;
 
 type Key = (Principal, String);
 
-/** struct Data(big_map::BigMap<Key, Principal>);
-
-impl Default for Data {
-    fn default() -> Self {
-        Self(big_map::BigMap::new(5, 2 * 1024 * 1024))
-    }
-}
-
 #[query]
 fn name() -> String {
     String::from("Dfinity Address Book")
 }
 
 #[update]
-async fn add_canister(key: String, value: Principal) {
+async fn get_canister(key: String) -> Option<Principal> {
     let pointer: Key = (caller(), key);
-    let data = storage::get_mut::<Data>();
-    data.0.insert(pointer, value).await;
+    // let data = storage::get::<Data>();
+    // return data.0.get(pointer).await;
 }
 
 #[update]
-async fn get_canister(key: String) -> Option<Principal> {
+async fn add_canister(key: String, value: Principal) {
     let pointer: Key = (caller(), key);
-    let data = storage::get::<Data>();
-    return data.0.get(pointer).await;
+    // let data = storage::get_mut::<Data>();
+    // data.0.insert(pointer, value).await;
 }
 
 #[update]
 async fn remove_canister(key: String) {
-
+    let pointer: Key = (caller(), key);
 }
 
 #[update]
-async fn remove_all(key: String) {
-
+async fn remove_all() {
 }
 
 #[update]
-async fn get_all(key: String) {
-
+async fn get_all() {
 }
-
-**/
