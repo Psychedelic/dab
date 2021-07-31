9d1c3c4f2eac2bea3ac93f8bcfff50ec9b6c1d0a
Nima Rasooli <nimarasooli1@gmail.com>
2021-08-01 01:43:18 +0430

    get_public_profile method.

--- profile/src/lib.rs
+++ profile/src/lib.rs
@@ -11,12 +11,14 @@ Every item in the map looks like this:
 ( UserID, UserProfileMetadata )
 **/
 
-struct ProfileMetadata {
-    display_name: String,
-    biography: String,
-    emoji: String,
-    avatar: String,
-    banner: String
+#[derive(Deserialize, CandidType)]
+pub struct ProfileMetadata {
+    display_name: Option<String>,
+    biography: Option<String>,
+    emoji: Option<String>,
+    avatar: Option<String>,
+    banner: Option<String>,
+    version: u32,
 }
 
 pub struct ProfileDB(BTreeMap<Principal, ProfileMetadata>);
@@ -27,9 +29,56 @@ impl Default for ProfileDB {
     }
 }
 
-impl ProfileDB {}
+impl ProfileDB {
+    pub fn get_public_profile(&mut self, account: &Principal) -> GetPublicProfileResult {
+        if self.0.contains_key(account) {
+            let profile = self.0.get(account).clone();
+            return GetPublicProfileResult {
+                profile_exists: true,
+                profile: profile,
+            };
+        }
+
+        GetPublicProfileResult {
+            profile_exists: false,
+            profile: None,
+        }
+    }
+
+    pub fn set_display_name(&mut self, account: Principal, name: String) {
+        assert_eq!(self.0.contains_key(&account), true);
+        ic_cdk::api::print(String::from("Works."));
+    }
+}
 
 #[query]
 fn name() -> String {
     String::from("Profile Canister")
 }
+
+#[derive(Deserialize, CandidType)]
+pub struct GetPublicProfileResult {
+    profile_exists: bool,
+    profile: Option<ProfileMetadata>,
+}
+
+#[update]
+fn get_public_profile(account: Option<Principal>) -> GetPublicProfileResult {
+    let profile_db = storage::get_mut::<ProfileDB>();
+    profile_db.get_public_profile(&account.unwrap_or_else(|| caller()))
+}
+
+#[update]
+fn set_display_name(name: String) {
+    let profile_db = storage::get_mut::<ProfileDB>();
+    profile_db.set_display_name(caller(), name);
+}
+
+#[update]
+fn set_biography() {}
+
+#[update]
+fn set_emoji() {}
+
+#[update]
+fn set_avatar() {}
