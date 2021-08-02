786d4c9937967305e5259162beff63eab7b56517
Nima Rasooli <nimarasooli1@gmail.com>
2021-08-02 18:41:30 +0430

    set_emoji is ready. Fixed some candid issues.

--- profile/src/lib.rs
+++ profile/src/lib.rs
@@ -1,8 +1,12 @@
+extern crate unic;
+
 use ic_cdk::export::candid::{CandidType, Principal};
 use ic_cdk::*;
 use ic_cdk_macros::*;
 use serde::Deserialize;
 use std::collections::BTreeMap;
+use unic::emoji::char::{is_emoji, Emoji};
+use unic::emoji::*;
 
 /**
 Every item in the map looks like this:
@@ -10,10 +14,13 @@ Every item in the map looks like this:
 ( UserID, UserProfileMetadata )
 **/
 
+// TODO: Every input should be checked. The length of description, the validity of avatar/banner url and ...
+// TODO: Set a policy for version control
+
 #[derive(Deserialize, CandidType, Clone)]
 pub struct ProfileMetadata {
     display_name: Option<String>,
-    biography: Option<String>,
+    description: Option<String>,
     emoji: Option<String>,
     avatar: Option<String>,
     banner: Option<String>,
@@ -39,24 +46,62 @@ impl ProfileDB {
                 x.display_name = Some(name);
             }
             None => {
-                    self.0.insert(
-                        account,
-                        ProfileMetadata {
-                            display_name: Some(name),
-                            biography: None,
-                            emoji: None,
-                            avatar: None,
-                            banner: None,
-                            version: 0,
-                        },
-                    );
-                }
+                self.0.insert(
+                    account,
+                    ProfileMetadata {
+                        display_name: Some(name),
+                        description: None,
+                        emoji: None,
+                        avatar: None,
+                        banner: None,
+                        version: 0,
+                    },
+                );
+            }
         }
     }
 
-    pub fn set_biography(&mut self, account: Principal, biography: String) {}
+    pub fn set_description(&mut self, account: Principal, description: String) {
+        match self.0.get_mut(&account) {
+            Some(x) => {
+                x.description = Some(description);
+            }
+            None => {
+                self.0.insert(
+                    account,
+                    ProfileMetadata {
+                        display_name: None,
+                        description: Some(description),
+                        emoji: None,
+                        avatar: None,
+                        banner: None,
+                        version: 0,
+                    },
+                );
+            }
+        }
+    }
 
-    pub fn set_emoji(&mut self, account: Principal, emoji: String) {}
+    pub fn set_emoji(&mut self, account: Principal, emoji: String) {
+        match self.0.get_mut(&account) {
+            Some(x) => {
+                x.emoji = Some(emoji);
+            }
+            None => {
+                self.0.insert(
+                    account,
+                    ProfileMetadata {
+                        display_name: None,
+                        description: None,
+                        emoji: Some(emoji),
+                        avatar: None,
+                        banner: None,
+                        version: 0,
+                    },
+                );
+            }
+        }
+    }
 
     pub fn set_avatar(&mut self, account: Principal, avatar: String) {}
 }
@@ -79,10 +124,18 @@ fn set_display_name(name: String) {
 }
 
 #[update]
-fn set_biography() {}
+fn set_description(description: String) {
+    let profile_db = storage::get_mut::<ProfileDB>();
+    profile_db.set_description(caller(), description);
+}
 
 #[update]
-fn set_emoji() {}
+fn set_emoji(input: String) {
+    let emojis: Vec<char> = input.chars().take(1).collect();
+    assert_eq!(is_emoji(emojis[0]), true);
+    let profile_db = storage::get_mut::<ProfileDB>();
+    profile_db.set_emoji(caller(), input);
+}
 
 #[update]
 fn set_avatar() {}
