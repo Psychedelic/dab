d9e4451b239e182dc09cfff12cf1811648e9ac55
Nima Rasooli <nimarasooli1@gmail.com>
2021-08-03 14:32:13 +0430

    Updated the shell script.

--- scripts/method-tests.sh
+++ scripts/method-tests.sh
@@ -1,78 +1,62 @@
 #!/bin/sh
 
-redBG=$(tput setab 1)
+redBG=$(tput setab 10)
+blackFG=$(tput setaf 0)
 reset=$(tput sgr0)
-action=$(echo "${redBG}"[ACTION]"${reset}")
+action=$(echo "${redBG}${blackFG}"[ACTION]"${reset}")
 
 # Step 0. Restarting DFX
 clear
-echo
-echo "${action} Restarting DFX"
-echo
+printf "\n%s Restarting DFX\n" "$action"
 cd ..
-dfx stop & rm -rf .dfx & dfx start --background --clean
+dfx stop & rm -rf .dfx & dfx start --background --clean --emulator
 
 # Step 1. Deploying DAB on IC
-echo
-echo "${action} Deploying DAB on IC"
-echo
+printf "\n%s Deploying canisters on IC\n" "$action" 
 dfx deploy
 
+tput setab 1
+printf "\nTESTING THE PRIVATE ADDRESS BOOK\n"
+tput sgr0
+
 # Step 2. Adding DAB's principal ID to the private address book using add_address method
-echo
-echo "${action} Adding DAB's principal ID to the BTree Map"
-echo
+printf "\n%s Adding DAB's principal ID to the BTree Map\n" "${action}"
 DAB=$(dfx canister id dab)
 dfx canister call dab add_address "(\"DAB\", principal \"$DAB\")"
 
 # Step 3. Getting the principal ID associated with the name "DAB" using get_address method
-echo
-echo "${action} Checking if DAB has been added to the map"
-echo
+printf "\n%s Checking if DAB has been added to the map\n" "${action}"
 dfx canister call dab get_address "(\"DAB\")"
 
 # Step 4. Adding another canister so that we can check the get_all method.
-echo
-echo "${action} Adding another address and calling the get_all method."
-echo
+printf "\n%s Adding another address and calling the get_all method.\n" "${action}"
 dfx canister call dab add_address "(\"XTC\", principal \"aanaa-xaaaa-aaaah-aaeiq-cai\")"
 dfx canister call dab get_all
 
 # Step 5. Creating another identity and adding addresses.
-echo
-echo "${action} Creating a new identity to make another private address book."
-echo
+printf "\n%s Creating a new identity to make another private address book.\n" "${action}"
 dfx identity new jack
 dfx identity use jack
 
 # Step 6. Asking DAB for another user's private address book.
-echo
-echo "${action} Checking if our new identity (Jack) can access other user's private data."
-echo
+printf "\n%s Checking if our new identity (Jack) can access other user's private data.\n" "${action}"
 dfx canister call dab get_address "(\"DAB\")"
 
 # Step 7. Adding a new address to Jack's address book.
-echo
-echo "${action} Adding DAB's address to Jack's address book."
-echo
+printf "\n%s Adding DAB's address to Jack's address book.\n" "${action}"
 dfx canister call dab add_address "(\"DAB\", principal \"$DAB\")"
 dfx canister call dab get_address "(\"DAB\")"
 
 # Step 8. Switching back to the other user and removing XTC
-echo
-echo "${action} Switching back to the previous user and removing the XTC canister address."
-echo
+printf "\n%s Switching back to the previous user and removing the XTC canister address.\n" "${action}"
 dfx identity use default
 dfx canister call dab remove_address "(\"XTC\")"
 dfx canister call dab get_all
 
 # Step n. Stopping the DFX replica
-echo
-echo "${action} Stopping DFX"
-echo
+printf "\n%s Stopping DFX.\n" "${action}"
 dfx stop
-wait $!
+sleep 1
 
-echo
-echo "${action} Exiting..."
+printf "\n%s Exiting.\n" "${action}"
 exit
