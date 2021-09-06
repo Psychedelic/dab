d7b25651a1539bbbaafcaaa700735606ce4ab9c1
Nima Rasooli <nimarasooli1@gmail.com>
2021-09-06 15:55:07 +0430

    Wrote the Add method.

--- nft/src/nft.rs
+++ nft/src/nft.rs
@@ -1,8 +1,56 @@
+use std::collections::HashMap;
 use ic_cdk::export::candid::{CandidType, Principal};
 use ic_cdk::*;
 use ic_cdk_macros::*;
+use serde::Deserialize;
+
+fn isController() -> bool {
+    true
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct NftCanister {
+    principal_id: Principal,
+    name: String,
+    standard: String
+}
+
+pub struct Registry(HashMap<String, NftCanister>);
+
+impl Default for Registry {
+    fn default() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl Registry {
+    pub fn add(&mut self, name: String, canister_info: NftCanister) -> String {
+        self.0.insert(name, canister_info);
+        String::from("Operation was successful.")
+    }
+}
 
 #[query]
 fn name() -> String {
     String::from("NFT Registry Canister")
+}
+
+#[update]
+fn add(canister_info: NftCanister) -> String {
+    if !isController() {
+        return String::from("You are not authorized to add and delete canisters.");
+    }
+
+    let name = canister_info.name.clone();
+    if name.len() <= 120 {
+        let db = storage::get_mut::<Registry>();
+        return db.add(name, canister_info);
+    }
+
+    String::from("The name of this canister has exceeded the limitation of 120 characters.")
+}
+
+#[update]
+fn get_all() {
+
 }
---
+++ scripts/nft-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dfx canister call nft add "(record {principal_id= principal \"aanaa-xaaaa-aaaah-aaeiq-cai\"; name= \"xtc\"; standard= \"icpunks\"})"
