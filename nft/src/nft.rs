7f7d0c36e53bdba684a3a76b90a494866331c340
Nima Rasooli <nimarasooli1@gmail.com>
2021-09-08 14:00:48 +0430

    Added edit and get_canister methods.

--- nft/src/nft.rs
+++ nft/src/nft.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use ic_cdk::export::candid::types::ic_types::principal;
 use ic_cdk::export::candid::{CandidType, Principal};
 use ic_cdk::*;
 use ic_cdk_macros::*;
@@ -29,10 +30,6 @@ impl Registry {
         String::from("Operation was successful.")
     }
 
-    pub fn get_all(&self) -> Vec<&NftCanister> {
-        self.0.values().collect()
-    }
-
     pub fn remove(&mut self, name: &String) -> String {
         if self.0.contains_key(name) {
             self.0.remove(name);
@@ -41,6 +38,29 @@ impl Registry {
 
         String::from("No such entry exists in the registry.")
     }
+
+    pub fn edit(&mut self, name: &String, principal_id: Option<Principal>, standard: Option<String>) -> String {
+        match self.0.get_mut(name) {
+            None => String::from("The canister you want to change does not exist in the registry."),
+            Some(canister) => {
+                if principal_id.is_some() {
+                    canister.principal_id = principal_id.unwrap();
+                } else {
+                    canister.standard = standard.unwrap();
+                }
+                return String::from("Operation was successful.")
+            },
+        }
+    }
+
+    pub fn get_canister(&self, name: &String) -> Option<&NftCanister> {
+        self.0.get(name)
+    }
+
+    pub fn get_all(&self) -> Vec<&NftCanister> {
+        self.0.values().collect()
+    }
+
 }
 
 #[query]
@@ -51,7 +71,7 @@ fn name() -> String {
 #[update]
 fn add(canister_info: NftCanister) -> String {
     if !is_controller() {
-        return String::from("You are not authorized to add and delete canisters.");
+        return String::from("You are not authorized to make changes.");
     }
 
     let name = canister_info.name.clone();
@@ -64,17 +84,37 @@ fn add(canister_info: NftCanister) -> String {
 }
 
 #[update]
-fn get_all() -> Vec<&'static NftCanister> {
-    let db = storage::get::<Registry>();
-    db.get_all()
-}
-
-#[update]
 fn remove(name: String) -> String {
     if !is_controller() {
-        return String::from("You are not authorized to add and delete canisters.");
+        return String::from("You are not authorized to make changes.");
     }
 
     let db = storage::get_mut::<Registry>();
     db.remove(&name)
+}
+
+#[update]
+fn edit(name: String, principal_id: Option<Principal>, standard: Option<String>) -> String {
+    if !is_controller() {
+        return String::from("You are not authorized to make changes.");
+    }
+    
+    if principal_id.is_none() && standard.is_none() {
+        return String::from("You should pass at least one of the principal_id or standard parameters.")
+    } else {
+        let db = storage::get_mut::<Registry>();
+        return db.edit(&name, principal_id, standard);
+    }
+}
+
+#[update]
+fn get_canister(name: String) -> Option<&'static NftCanister> {
+    let db = storage::get::<Registry>();
+    db.get_canister(&name)
+}
+
+#[update]
+fn get_all() -> Vec<&'static NftCanister> {
+    let db = storage::get::<Registry>();
+    db.get_all()
 }
