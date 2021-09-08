5825a08b35839d41a80e68604f65508e0d9a0c9b
Nima Rasooli <nimarasooli1@gmail.com>
2021-09-08 18:43:37 +0430

    Integrated the ic_kit library and wrote the controller test.

--- nft/src/nft.rs
+++ nft/src/nft.rs
@@ -1,15 +1,30 @@
-use ic_cdk::export::candid::types::ic_types::principal;
 use ic_cdk::export::candid::{CandidType, Principal};
 use ic_cdk::*;
 use ic_cdk_macros::*;
+use ic_kit::macros::*;
+use ic_kit::*;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-fn is_controller() -> bool {
-    true
+struct Controller(Principal);
+impl Default for Controller {
+    fn default() -> Self {
+        panic!("Cannot set a default controller!")
+    }
 }
 
-#[derive(CandidType, Deserialize)]
+#[init]
+fn init() {
+    let ic = get_context();
+    let controller = ic.caller();
+    ic.store(Controller(controller));
+}
+
+fn is_controller(account: &Principal) -> bool {
+    account == &get_context().get::<Controller>().0
+}
+
+#[derive(CandidType, Deserialize, Clone)]
 pub struct NftCanister {
     principal_id: Principal,
     name: String,
@@ -69,13 +84,14 @@ fn name() -> String {
 
 #[update]
 fn add(canister_info: NftCanister) -> String {
-    if !is_controller() {
+    let ic = get_context();
+    if !is_controller(&ic.caller()) {
         return String::from("You are not authorized to make changes.");
     }
 
     let name = canister_info.name.clone();
     if name.len() <= 120 {
-        let db = storage::get_mut::<Registry>();
+        let db = ic.get_mut::<Registry>();
         return db.add(name, canister_info);
     }
 
@@ -84,17 +100,19 @@ fn add(canister_info: NftCanister) -> String {
 
 #[update]
 fn remove(name: String) -> String {
-    if !is_controller() {
+    let ic = get_context();
+    if !is_controller(&ic.caller()) {
         return String::from("You are not authorized to make changes.");
     }
 
-    let db = storage::get_mut::<Registry>();
+    let db = ic.get_mut::<Registry>();
     db.remove(&name)
 }
 
 #[update]
 fn edit(name: String, principal_id: Option<Principal>, standard: Option<String>) -> String {
-    if !is_controller() {
+    let ic = get_context();
+    if !is_controller(&ic.caller()) {
         return String::from("You are not authorized to make changes.");
     }
 
@@ -103,19 +121,51 @@ fn edit(name: String, principal_id: Option<Principal>, standard: Option<String>)
             "You should pass at least one of the principal_id or standard parameters.",
         );
     } else {
-        let db = storage::get_mut::<Registry>();
+        let db = ic.get_mut::<Registry>();
         return db.edit(&name, principal_id, standard);
     }
 }
 
 #[update]
 fn get_canister(name: String) -> Option<&'static NftCanister> {
-    let db = storage::get::<Registry>();
+    let ic = get_context();
+    let db = ic.get_mut::<Registry>();
     db.get_canister(&name)
 }
 
 #[update]
 fn get_all() -> Vec<&'static NftCanister> {
-    let db = storage::get::<Registry>();
+    let ic = get_context();
+    let db = ic.get_mut::<Registry>();
     db.get_all()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ic_kit::*;
+
+    #[test]
+    fn test_controller() {
+        // alice is the controller
+        let ctx = MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        init();
+
+        let canister_info = NftCanister {
+            name: String::from("test_canister"),
+            principal_id: mock_principals::xtc(),
+            standard: String::from("Dank")
+        };
+
+        let mut addition = add(canister_info.clone());
+        assert_eq!(addition, String::from("Operation was successful."));
+        remove(String::from("test_canister"));
+
+        ctx.update_caller(mock_principals::bob());
+        addition = add(canister_info);
+        assert_eq!(addition, String::from("You are not authorized to make changes."));
+    }
 }
