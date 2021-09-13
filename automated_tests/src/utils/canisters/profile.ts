11fb1c5455a973b295c60bfd04f5d06d5d7066b6
Nima Rasooli <nimarasooli1@gmail.com>
2021-09-13 18:10:58 +0430

    Automated tests (#13)
    * new branch
    
    * Added method tests
    
    * Null case.
    
    * Minor fixes.
    
    * Removed .eslintcache
    
    * Updated the tests.
    
    * Removed import

---
+++ automated_tests/src/utils/canisters/profile.ts
@@ -0,0 +1,15 @@
+import { Actor } from '@dfinity/agent';
+import agent from '../agent';
+import { ProfileIDL, ProfileIDLFactory } from '../idls';
+
+const createActor = (canisterId: string) => {
+  const actor = Actor.createActor<ProfileIDL>(ProfileIDLFactory, {
+    canisterId,
+    agent,
+  });
+  return actor;
+};
+
+export default createActor;
+
+export const getProfileCanisterId = 'ryjl3-tyaaa-aaaaa-aaaba-cai';
