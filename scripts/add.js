9873e639b42f2cb36bdd9c9fb11eb0a52546a377
Nima Rasooli <nimarasooli1@gmail.com>
2021-09-29 18:44:51 +0330

    Updated the add script.

--- scripts/add.js
+++ scripts/add.js
@@ -5,25 +5,48 @@ const fs = require("fs"),
       program = require("inquirer"),
       { execSync } = require('child_process');
 
-var results = [];
+var results = [],
+    inputFile = process.argv[2],
+    ui = new program.ui.BottomBar();
 
-var inputFile = process.argv[2];
-
-let stream = fs.createReadStream(inputFile)
+function main() {
+    program
+        .prompt([
+            {
+                type: 'list',
+                name: 'functionality',
+                message: 'What do you want to do?',
+                choices: [
+                    'Add / Update NFT canister',
+                    'Add / Update entries in the registry canister'
+                ]
+            }
+        ])
+        .then((answer) => {
+            let stream = fs.createReadStream(inputFile)
                 .pipe(csv());
 
-stream
+            if (answer.functionality == 'Add / Update NFT canister') {
+                add_nft(stream);
+            } else {
+                add_canister(stream);
+            }
+        });
+}
+
+function add_nft(stream) {
+    stream
     .on("data", (data) => {
-        if (data.name != '' && data.standard != '' && data.id != '' && data.description != '' && data.icon != '') {
-            let item = {
-                name: data.name,
-                id: data.id,
-                standard: data.standard,
-                description: data.description,
-                icon: data.icon,
-            };
-            results.push(item);
-        }
+            if (data.name != '' && data.standard != '' && data.id != '' && data.description != '' && data.icon != '') {
+                let item = {
+                    name: data.name,
+                    id: data.id,
+                    standard: data.standard,
+                    description: data.description,
+                    icon: data.icon,
+                };
+                results.push(item);
+            }
     })
     .on("end", () => {
         console.log(results);
@@ -55,14 +78,92 @@ stream
                             'dfx',
                             'canister',
                             '--network=ic',
+                            '--no-wallet',
                             'call',
                             answers.address,
                             'add',
                             `"(record {principal_id= principal \\"${id}\\"; name= \\"${name}\\"; standard= \\"${standard}\\"; description= \\"${description}\\"; icon= \\"${icon}\\"})"`,
                         ];
-                        execSync(command.join(' '));
-                        console.log(`Add: ${name}`);
+                        try {
+                            execSync(command.join(' '));
+                        } catch (e) {
+                            ui.log.write(`FAILED: ${name}`);
+                            continue;
+                        }
+                        ui.log.write(`ADDED: ${name}`);
+                        ui.updateBottomBar(`${i + 1}/${results.length + 1}`);
                     }
+                    ui.updateBottomBar('FINISHED');
+                } 
+            });
+    });
+
+}
+
+
+function add_canister(stream) {
+    stream
+    .on("data", (data) => {
+            if (data.Name != '' && data.Description != '' && data.URL != '' && data.Logo != '' && data.ID != '') {
+                let item = {
+                    name: data.Name,
+                    id: data.ID,
+                    description: data.Description,
+                    url: data.URL,
+                    logo: data.Logo,
+                };
+                results.push(item);
+            }
+    })
+    .on("end", () => {
+        console.log(results);
+        program
+            .prompt([
+                {
+                    type: 'confirm',
+                    name: 'isThisOk',
+                    message: 'Does this parsing look okay?',
+                    default: false,
+                },
+                {
+                    type: 'input',
+                    name: 'address',
+                    message: "Please enter canister's principal ID: "
+                }
+            ])
+            .then((answers) => {
+                if (answers.isThisOk) {
+                    for (let i = 0; i < results.length; i++) {
+                        let canister = results[i],
+                            name = canister.name,
+                            description = canister.description,
+                            id = canister.id,
+                            url = canister.url,
+                            logo = canister.logo;
+                        
+                        const command = [
+                            'dfx',
+                            'canister',
+                            '--network=ic',
+                            '--no-wallet',
+                            'call',
+                            answers.address,
+                            'add_canister',
+                            `"(principal \\"${id}\\", record {name= \\"${name}\\"; url= \\"${url}\\"; description= \\"${description}\\"; logo_url= \\"${logo}\\"})"`,
+                        ];
+                        try {
+                            execSync(command.join(' '));
+                        } catch (e) {
+                            ui.log.write(`FAILED: ${name}`);
+                            continue;
+                        }
+                        ui.log.write(`ADDED: ${name}`);
+                        ui.updateBottomBar(`${i + 1}/${results.length + 1}`);
+                    }
+                    ui.updateBottomBar('FINISHED');
                 }
             });
     });
+}
+
+main();
