2a83f38b35771c62f56b24b01ee16644c9e06fd8
migue toscano <miguetoscano288@gmail.com>
2022-03-01 22:38:38 -0300

    Added tests

--- registries/address_book/src/address_book.rs
+++ registries/address_book/src/address_book.rs
@@ -123,3 +123,142 @@ fn get_all() -> Vec<&'static Address> {
         .map(|entry| entry.1)
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_address_successfully() {
+        MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        let address_info = Address {
+            name: String::from("Bob"),
+            description: Some(String::from("Friend")),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let addition_result = add(address_info.clone());
+        assert!(addition_result.is_ok());
+
+        let addresses = get_all();
+        assert_eq!(addresses.len(), 1);
+    }
+
+    #[test]
+    fn test_add_address_fails_because_of_long_description_param() {
+        MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        let address_info = Address {
+            name: String::from("Bob"),
+            description: Some(std::iter::repeat("X").take(1201).collect::<String>()),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let addition_result = add(address_info.clone());
+        assert!(addition_result.is_err());
+        assert_eq!(addition_result.unwrap_err(), Failure::BadParameters);
+    }
+
+    #[test]
+    fn test_add_address_fails_because_of_long_name_param() {
+        MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        let address_info = Address {
+            name: std::iter::repeat("X").take(25).collect::<String>(),
+            description: Some(String::from("description")),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let addition_result = add(address_info.clone());
+        assert!(addition_result.is_err());
+        assert_eq!(addition_result.unwrap_err(), Failure::BadParameters);
+    }
+
+    #[test]
+    fn test_add_address_fails_because_of_bad_emoji_param() {
+        MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        let address_info = Address {
+            name: String::from("Bob"),
+            description: Some(String::from("description")),
+            emoji: Some(String::from("a")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let addition_result = add(address_info.clone());
+        assert!(addition_result.is_err());
+        assert_eq!(addition_result.unwrap_err(), Failure::BadParameters);
+    }
+
+    #[test]
+    fn test_remove_address_successfully() {
+        MockContext::new()
+            .with_caller(mock_principals::alice())
+            .inject();
+
+        let address_info = Address {
+            name: String::from("Bob"),
+            description: Some(String::from("Friend")),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let addition_result = add(address_info.clone());
+        assert!(addition_result.is_ok());
+
+        let removal_result = remove(String::from("Bob"));
+        assert!(removal_result.is_ok());
+
+        let get_all_result = get_all();
+        assert_eq!(get_all_result.len(), 0);
+    }
+
+    #[test]
+    fn test_users_get_their_own_addresses() {
+        let context = MockContext::new().inject();
+
+        let bob_address_info = Address {
+            name: String::from("Bob"),
+            description: Some(String::from("Friend")),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::bob(),
+        };
+
+        let alice_address_info = Address {
+            name: String::from("Alice"),
+            description: Some(String::from("Friend")),
+            emoji: Some(String::from("😚")),
+            principal_id: mock_principals::alice(),
+        };
+
+        //Alice adds Bob as her contact
+        context.update_caller(mock_principals::alice());
+        add(bob_address_info);
+
+        let alice_addresses = get_all();
+
+        assert_eq!(alice_addresses.len(), 1);
+        assert_eq!(alice_addresses[0].name, String::from("Bob"));
+
+        //Bob adds Alison as his contact
+        context.update_caller(mock_principals::bob());
+        add(alice_address_info);
+
+        let bob_addresses = get_all();
+
+        assert_eq!(bob_addresses.len(), 1);
+        assert_eq!(bob_addresses[0].name, String::from("Alice"));
+    }
+}
