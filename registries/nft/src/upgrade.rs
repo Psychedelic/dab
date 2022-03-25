913274bbd7737f0761fd871562a3787471f94724
migue toscano <miguetoscano288@gmail.com>
2022-03-25 09:05:14 -0300

    updated upgrade script

--- registries/nft/src/upgrade.rs
+++ registries/nft/src/upgrade.rs
@@ -1,4 +1,4 @@
-use crate::nft::{Controller, DetailValue, NftCanister, Registry};
+use crate::nft::{Controller, DetailValue, NftCanister, Registry };
 use ic_cdk::export::candid::{CandidType, Deserialize, Principal};
 use ic_kit::ic::*;
 use ic_kit::macros::*;
@@ -31,7 +31,24 @@ pub fn pre_upgrade() {
 #[post_upgrade]
 pub fn post_upgrade() {
     if let Ok((stable,)) = ic::stable_restore::<(StableStorage,)>() {
-        ic::get_mut::<Registry>().load(stable.db);
+        let mut updated_nft_canisters = Vec::with_capacity(stable.db.len());
+
+        for (_key, nft_canister) in stable.db.into_iter() {
+            let updated_nft_canister = NftCanister {
+                name: nft_canister.name.clone(),
+                description: nft_canister.description.clone(),
+                thumbnail: nft_canister.thumbnail.clone(),
+                frontend: nft_canister.frontend.clone(),
+                principal_id: nft_canister.principal_id.clone(),
+                details: vec![
+                    nft_canister.details[0].clone(),
+                    (String::from("asset_type"), DetailValue::Text(String::from("ASSET TYPE HERE"))),
+                ],
+            };
+
+            updated_nft_canisters.push((updated_nft_canister.principal_id, updated_nft_canister));
+        }
+        ic::get_mut::<Registry>().load(updated_nft_canisters);
         ic::store(Controller(stable.controller));
     }
 }
