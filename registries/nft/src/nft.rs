39ba9b7d52d8ad123e09f99012d0b4279a96a230
migue toscano <miguetoscano288@gmail.com>
2022-05-27 01:25:07 -0300

    Updated nft

--- registries/nft/src/nft.rs
+++ registries/nft/src/nft.rs
@@ -33,9 +33,30 @@ impl Registry {
     ) -> Result<(), OperationError> {
         let nft = self.0.get(&canister_info.principal_id);
 
-        if nft.is_some() && nft.unwrap().submitter != *caller {
+        // If its an update, check if the caller matches the submitter or if its an admin
+        if nft.is_some() && !is_admin(caller) && nft.unwrap().submitter != *caller {
             return Err(OperationError::NotAuthorized);
-        } else {
+        }
+ 
+        // An admin can update any entry
+        if nft.is_some() && is_admin(caller) {
+            let updated_nft = NftCanister {
+                name: canister_info.name,
+                description: canister_info.description,
+                thumbnail: canister_info.thumbnail,
+                frontend: canister_info.frontend,
+                principal_id: canister_info.principal_id,
+                submitter: nft.unwrap().submitter,
+                last_updated_by: *caller,
+                last_updated_at: ic::time(),
+                details: canister_info.details.clone(),
+            };
+
+            self.0.insert(canister_info.principal_id, updated_nft);
+        }
+
+        // Its a new entry
+        else {
             let new_nft = NftCanister {
                 name: canister_info.name,
                 description: canister_info.description,
@@ -65,7 +86,7 @@ impl Registry {
 
         let nft = self.0.get(principal_id).unwrap();
 
-        if nft.submitter != *caller {
+        if nft.submitter != *caller && !is_admin(caller) {
             return Err(OperationError::NotAuthorized);
         }
 
@@ -89,8 +110,9 @@ fn name() -> String {
 }
 
 #[update]
-pub async fn add(caller: Principal, canister_info: AddNftInput) -> Result<(), OperationError> {
-    if !is_admin(&ic::caller()) {
+pub async fn add(trusted_source: Option<Principal>, canister_info: AddNftInput) -> Result<(), OperationError> {
+    let caller = ic::caller();
+    if !is_admin(&caller) {
         return Err(OperationError::NotAuthorized);
     } else if !validate_url(&canister_info.thumbnail) {
         return Err(OperationError::BadParameters);
@@ -124,20 +146,21 @@ pub async fn add(caller: Principal, canister_info: AddNftInput) -> Result<(), Op
         };
 
         let db = ic::get_mut::<Registry>();
-        return db.add(&caller, canister_info);
+        return db.add(&trusted_source.unwrap_or(caller), canister_info);
     }
 
     Err(OperationError::BadParameters)
 }
 
 #[update]
-pub fn remove(caller: Principal, principal_id: Principal) -> Result<(), OperationError> {
-    if !is_admin(&ic::caller()) {
+pub fn remove(trusted_source: Option<Principal>, principal_id: Principal) -> Result<(), OperationError> {
+    let caller = ic::caller();
+    if !is_admin(&caller) {
         return Err(OperationError::NotAuthorized);
     }
 
     let db = ic::get_mut::<Registry>();
-    db.remove(&caller, &principal_id)
+    db.remove(&trusted_source.unwrap_or(caller), &principal_id)
 }
 
 #[query]
